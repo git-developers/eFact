6e488e3d22a72eb1c81d524a80103e3a91cb93d2
Tianos <tianos@gmail.com>
2019-03-18 14:07:17 -0500

    Tianos said -> modifications made on: 2019-03-18 . 14:07:17

--- src/com/efact/dao/oracle/PaymentVoucherImpDao.java
+++ src/com/efact/dao/oracle/PaymentVoucherImpDao.java
@@ -62,86 +62,16 @@ public class PaymentVoucherImpDao extends OracleDaoFactory implements PaymentVou
             objectOut.setListPaymentTipoDoi(listTipoDoi);
 
             
-            /**
-             * Cursor: Tipo Comprobante
-             */
-            ResultSet rsTipoComprobante = (ResultSet) st.getObject(2);
-            List<PaymentTipoComprobante> listTipoComprobante = new ArrayList<PaymentTipoComprobante>();
-            while (rsTipoComprobante.next()) {
-            	PaymentTipoComprobante o = new PaymentTipoComprobante();
-            	o.setCscTipo(rsTipoComprobante.getInt("CSC_TIPO"));
-            	o.setCscTipoNombre(rsTipoComprobante.getString("CSC_TIPO_NOMBRE"));
-                listTipoComprobante.add(o);
-            }
-            objectOut.setListPaymentTipoComprobante(listTipoComprobante);
-
-            
-            /**
-             * Cursor: Tipo Moneda
-             */
-            ResultSet rsTipoMoneda = (ResultSet) st.getObject(3);
-            List<PaymentTipoMoneda> listTipoMoneda = new ArrayList<PaymentTipoMoneda>();
-            while (rsTipoMoneda.next()) {
-            	PaymentTipoMoneda o = new PaymentTipoMoneda();
-            	o.setIdMoneda(rsTipoMoneda.getInt("ID_MONEDA"));
-            	o.setDescripcion(rsTipoMoneda.getString("DESCRIPCION"));    		    
-            	o.setNombreCorto(rsTipoMoneda.getString("NOMBRE_CORTO"));  
-            	listTipoMoneda.add(o);
-            }                
-            objectOut.setListPaymentTipoMoneda(listTipoMoneda);            
-
-            
-            /**
-             * Cursor: Cuota
-             */
-//            ResultSet rsCuota = (ResultSet) st.getObject(4);
-//            List<PaymentCuota> listCuota = new ArrayList<PaymentCuota>();
-//            while (rsCuota.next()) {
-//            	PaymentCuota o = new PaymentCuota();
-//            	//o.setXxxxxxxxxx(rsTipoMoneda.getString("XXXXXXXX")); 
-//            	listCuota.add(o);
-//            }                
-//            objectOut.setListPaymentCuota(listCuota);  
-            
-            
-            /**
-             * Cursor: Recaudo
-             */
-            ResultSet rsRecaudo = (ResultSet) st.getObject(4);
-            List<PaymentRecaudo> listRecaudo = new ArrayList<PaymentRecaudo>();
-            while (rsRecaudo.next()) {
-            	PaymentRecaudo o = new PaymentRecaudo();
-            	o.setIdRecaudo(rsRecaudo.getInt("ID_RECAUDO"));
-            	o.setDescripcion(rsRecaudo.getString("DESCRIPCION"));
-            	listRecaudo.add(o);
-            }                
-            objectOut.setListPaymentRecaudo(listRecaudo);  
-            
-            
-            /**
-             * Cursor: Concepto
-             */
-            ResultSet rsConcepto = (ResultSet) st.getObject(5);
-            List<PaymentConcepto> listConcepto = new ArrayList<PaymentConcepto>();
-            while (rsConcepto.next()) {
-            	PaymentConcepto o = new PaymentConcepto();
-            	o.setIdRecaudo(rsConcepto.getInt("ID_RECAUDO"));
-            	o.setIdConcepto(rsConcepto.getInt("ID_CONCEPTO"));
-            	o.setDescripcion(rsConcepto.getString("DESCRIPCION"));
-            	listConcepto.add(o);
-            }                
-            objectOut.setListPaymentConcepto(listConcepto);  
             
             
             /**
              * Close
              */
-//            rsCuota.close();
             rsTipoDoi.close();
-            rsRecaudo.close();
-            rsConcepto.close();
-            rsTipoMoneda.close();
-            rsTipoComprobante.close();
+//            rsRecaudo.close();
+//            rsConcepto.close();
+//            rsTipoMoneda.close();
+//            rsTipoComprobante.close();
             st.close();
             
         } catch (Exception e){

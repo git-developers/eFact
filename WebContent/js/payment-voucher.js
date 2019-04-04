13596b4b4b716ace700d15ac356ad330f747744f
Tianos <tianos@gmail.com>
2019-04-04 05:23:26 -0500

    Tianos said -> modifications made on: 2019-04-04 . 05:23:26

--- WebContent/js/payment-voucher.js
+++ WebContent/js/payment-voucher.js
@@ -149,9 +149,16 @@
         
         function fillForm(data) {
         	
+        	console.dir(" ***** fillForm ******* ");
+        	console.dir(data);
+        	
         	data = JSON.parse(data);
         	
-        	if (data.listPaymentDetail.length <= 0) {
+        	console.log(" ******** AFTER ******** ");
+        	console.dir(data);
+        	
+        	
+        	if (typeof data.listPaymentDetail === "undefined" || data.listPaymentDetail.length <= 0) {
         		return false;
         	}
         	

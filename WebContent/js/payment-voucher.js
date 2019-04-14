e88792d9a7ed704f84fe60280a281d070951937b
Tianos <tianos@gmail.com>
2019-04-14 15:53:51 -0500

    Tianos said -> modifications made on: 2019-04-14 . 15:53:51

--- WebContent/js/payment-voucher.js
+++ WebContent/js/payment-voucher.js
@@ -181,7 +181,7 @@
         	
         	if (flagTipo == "5") {
         		console.log("flagTipo -> 5");
-        		$("input[name=queryNumeroDoi]").attr('onkeyup', " this.value = /^\d*$/.test(this.value) ? this.value : ''; console.log(/^\d*$/.test(this.value)); ");
+        		$("input[name=queryNumeroDoi]").attr('onkeyup', " console.log(/^\d*$/.test(this.value)); this.value = /^\d*$/.test(this.value) ? this.value : '';  ");
         	}
         	
         };
